e7c6b95f4eff9daf0e81cab4aad0da6042f582e3
deploy-every <betterpandey@gmail.com>
2025-03-03 15:23:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import numpy as np
-
+import uvicorn
 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*"
-    ],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 with open("phishing_detection_model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
@@ -31,9 +32,12 @@ class EmailInput(BaseModel):
 @app.post("/predict")
 def predict_email(input_data: EmailInput):
     input_features = vectorizer.transform([input_data.message])
-
     prediction = model.predict(input_features)
-
     result = "Legitimate mail" if prediction[0] == 1 else "Phishy mail"
-
     return {"message": input_data.message, "classification": result}
+
+
+if __name__ == "__main__":
+    # Dynamic port binding for deployment
+    port = int(os.getenv("PORT", 8000))  
+    uvicorn.run(app, host="0.0.0.0", port=port)
